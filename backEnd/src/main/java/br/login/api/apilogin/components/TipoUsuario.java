3479ef6b4c613af0b37dc466a342dd1794f68d57
Vinícius Pereira Polli <viniciusppolli07@gmail.com>
2025-06-24 15:45:03 -0300

    Versão final

--- backEnd/src/main/java/br/login/api/apilogin/components/TipoUsuario.java
+++
@@ -1,7 +0,0 @@
-package br.login.api.apilogin.components;
-
-public enum TipoUsuario {
-    Admin,
-    AgenteSaude,
-    Cidadao;
-}
--- backEnd/src/main/java/br/login/api/apilogin/components/ValidadorEmail.java
+++
@@ -1,4 +0,0 @@
-package br.login.api.apilogin.components;
-
-public class ValidadorEmail {
-}
